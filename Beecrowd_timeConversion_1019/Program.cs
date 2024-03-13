cb24aed8ffd53beec50af127a7365250572a478c
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-03-13 18:56:08 +0600

    Add project files.

---
+++ Beecrowd_timeConversion_1019/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beecrowd_timeConversion_1019
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int temp;
+
+            temp = int.Parse(Console.ReadLine());
+
+            int hour = temp / 3600;
+            temp = temp - (hour * 3600);
+
+            int min = temp / 60;
+            temp = temp - (min * 60);
+
+            Console.WriteLine("{0}:{1}:{2}", hour, min, temp);
+            Console.ReadKey();
+        }
+    }
+}
